b44ac19dc149edf3c59f1165e6c0f70e4f9db986
Hejifei <657414658@qq.com>
2018-07-09 11:34:31 +0800

    1

--- src/pages/Layout/Layout.vue
+++ src/pages/Layout/Layout.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="hello">
-    <el-container>
-      <el-header>Header</el-header>
-      <el-main>
-        普通公共界面
-        <router-view/>
-      </el-main>
-      <el-footer>Footer</el-footer>
+    <el-container class='containerC'>
+      <el-header class='BMhead'>
+        <el-row :gutter="20">
+          <el-col :span="4">LOGO</el-col>
+          <el-col :span="16" class='BMheadmid'>
+            <img />富士通人脸识别通道管理
+          </el-col>
+          <el-col :span="4" class='BMheadR'>
+            <el-dropdown>
+              <span class="el-dropdown-link">
+                <i class="icon-camera-retro"></i>
+                <i class="el-icon-edit"></i>
+                用户名xxx
+                <img />
+                <!-- <i class="el-icon-arrow-down el-icon--right"></i> -->
+              </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>修改密码</el-dropdown-item>
+                <el-dropdown-item>退出</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>
+          <router-view/>
+        </el-main>
+      </el-container>
     </el-container>
-    
   </div>
 </template>
 
@@ -24,4 +46,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.containerC{height:100vh;}
+.BMhead{background: linear-gradient(to right, #006DB3 , #3CA0D0);color:#fff;text-align:left;}
+.BMheadmid{text-align:left;padding-left:20px !important;}
+.BMheadmid img{margin:0 20px;}
+.BMheadR{color:#fff;text-align:right;}
+.BMheadR .el-dropdown-link{color:#fff;font-size:12px;}
+.BMheadR .el-dropdown-link i:nth-child(1){font-size:12px;width:16px;height:16px;display: inline-block;}
 </style>
